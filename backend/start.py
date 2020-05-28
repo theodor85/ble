bbf83f2d776736b5eb8bbc04e666fcf2db7f6e89
Fedor Kosolapov <fedor_coder@mail.ru>
2020-05-28 13:45:00 +0700

    'source' service comlete

--- backend/start.py
+++ backend/start.py
@@ -1,13 +1,31 @@
+import logging
+
 from aiohttp import web
+
+
+logger = logging.getLogger("backend")
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+logger.addHandler(handler)
+
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
     return web.Response(text=text)
 
+async def get_points(request):
+    logger.info(f'Data was received! {request}')
+    return web.Response(text='OK')
+
+
 app = web.Application()
 app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+                web.get('/{name}', handle),
+                web.post('/points', get_points)])
 
 if __name__ == '__main__':
     web.run_app(app)
--- source/start.py
+++ source/start.py
@@ -1,4 +1,82 @@
+''' Модуль отправляет тестовые данные о положении меток.
+    Позже может быть замменен на библиотеки, работающие с реальными данными.
+'''
+
+import logging
+import time
+import json
+
+import requests
+from requests.exceptions import RequestException
+
+
+TEST_DATA = [
+    [51, 51], # 1
+    [63, 45], # 2
+    [76, 42], # 3
+    [89, 45], # 4
+    [86, 58], # 5
+    [84, 72], # 6
+    [86, 86], # 7
+    [89, 100], # 8
+    [76, 99], # 9
+    [67, 92], # 10
+    [64, 78], # 11
+    [64, 64], # 12
+    [50, 67], # 13
+    [45, 63], # 14
+]
+
+BACKEND_URL = "http://backend:8080/points"
+SENDING_INTERVAL = 3 # сек
+
+logger = logging.getLogger("source")
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+logger.addHandler(handler)
+
+def make_body_request(r_list):
+    ''' Функция возвращает тело post-запроса к бэкенду.
+        На вход получает список с расстояниями от якорей до метки
+    '''
+    return {
+        "ble_points": [
+            {
+                "id_point": "point1",
+                "anchors_data": [
+                    {
+                        "anchor1":r_list[0]
+                    },
+                    {
+                        "anchor2":r_list[1]
+                    },
+                ]
+            },
+        ]
+    }
+
 
 if __name__ == "__main__":
-    print("********Source started!")
+    logger.info("******** Source started!")
 
+    i = 0
+    while True:
+        time.sleep(SENDING_INTERVAL)
+        try:
+            body = make_body_request(TEST_DATA[i])
+            response = requests.post(BACKEND_URL, data=json.dumps(body))
+        except RequestException as req_exception:
+            msg = f"******** Backend unreachable! ERROR: {req_exception}"
+            logger.error(msg)
+        else:
+            if response.status_code != 200:
+                msg = f"******** Backend returned error status code: {response.status_code}"
+                logger.error(msg)
+            msg = f"Points data was sent: {TEST_DATA[i]}"
+            logger.info(msg)
+        i += 1
+        if i >= 14:
+            i = 0
