089bee612ba1719b904f7beded6c0b269668b7dc
Fedor Kosolapov <fedor_coder@mail.ru>
2020-05-29 10:30:24 +0700

    fix canvas draw

--- frontend/script.js
+++ frontend/script.js
@@ -7,12 +7,6 @@ sock.onopen = function (event) {
 
 sock.onclose = function (event) {
     console.log(event);
-    if(event.wasClean){
-        console.log('Clean connection end');
-    } else {
-        console.log('Connection broken');
-    }
-    window.location.assign('/');
 };
 
 sock.onerror = function (error) {
@@ -22,40 +16,17 @@ sock.onerror = function (error) {
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
 
-sock.onmessage = function showMessage(message) {
-    var str_data = JSON.parse(message.data);
-    var data = JSON.parse(str_data)
+sock.onmessage = function draw(message) {
+    var coords = JSON.parse(JSON.parse(message.data))
     // умножаем на три, т.к. у нас сторона 300х300, а на сервере - 100х100
-    console.log(typeof(data))
-    console.log(data)
-    let x = 3*data.x;
-    let y = 3*data.y;
-
+    let x = 3*coords.x;
+    let y = 3*coords.y;
     console.log('x = ' + x + '; y = ' + y)
 
-    ctx.moveTo(x-5, y);
+    ctx.clearRect(0,0,300,300);
+    
+    ctx.beginPath();
+    ctx.fillStyle = 'green';
     ctx.arc(x, y, 5, 0, Math.PI*2);
-    ctx.stroke();
-
+    ctx.fill();
 }
-
-// function draw(){
-//     var canvas = document.getElementById('canvas');
-    
-    
-
-//     if (canvas.getContext){
-//         console.log("Inside the function!")
-//         var ctx = canvas.getContext("2d");
-
-//         ctx.beginPath();
-//         
-
-        
-        
-//         ctx.stroke();
-//     }
-//     else{
-//         console.log("canvas не поддерживается!")
-//     }
-//   }
